d841ef75ef6f3bd23082dab94ca8f40346ba6d12
Vycheslav Smirnov <smirn.006vychislavoo@gmail.com>
2023-03-08 12:02:55 +0300

    Added clustering I'm dealing with using localStorage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+geoObjects = [];
+const storage = localStorage;
+
 function init() {
     const map = new ymaps.Map('map', {
         center: [59.94, 30.32],
@@ -7,14 +10,47 @@ function init() {
         noPlacemark: true
     });
 
-    const reviewsCollections = new ymaps.GeoObjectCollection({}, {
-        draggable: false
-    });
+    const clusterer = new ymaps.Clusterer({
+        groupByCoordinates: true,
+        clusterDisableClickZoom: true,
+        hideIconOnBalloonOpen: false,
 
-    const balloon = new ymaps.Balloon(map, {
-        minHeight: 500
+        // clusterIconContentLayout:
     })
-    balloon.open(map.getCenter());
+
+    for (const coords in JSON.parse(storage.geoObjects || '{}')) {
+        let reviewItems = '';
+        console.log(coords)
+        console.log(storage.geoObjects[coords])
+        console.log(storage.geoObjects)
+        for (const review of storage.geoObjects[coords]) {
+            reviewItems += [
+                '<li class="balloon__reviews-item">',
+                `<span class="item__name">${review.name}</span>`,
+                `<span class="item__information">${review.place}</span>`,
+                `<p class="item__review">${review.review}</p>`,
+                '</li>'].join('')
+        }
+
+        geoObjects.push(new ymaps.Placemark(coords, {
+            balloonContent: [
+                '<div class="balloon">',
+                '<ul class="balloon__reviews-list">',
+                reviewItems,
+                '</ul>',
+                '<h3 class="balloon__title">Отзыв:</h3>',
+                '<form class="balloon__form" action="#">',
+                '<input class="input__info" type="text" placeholder="Укажите ваше имя">',
+                '<input class="input__info" type="text" placeholder="Укажите место">',
+                '<textarea  class="input__info input__review" name="review" cols="30" rows="10" placeholder="Оставить отзыв"></textarea>',
+                '<button class="balloon__form-button" type="submit">Добавить</button>',
+                '</form>',
+                '</div>'
+            ].join(''),
+        }, {balloonMinHeight: 450}));
+
+        clusterer.add(geoObjects);
+    }
 
     document.addEventListener('keydown', (e) => {
         if (e.code === 'Space') {
@@ -28,10 +64,51 @@ function init() {
         }
     });
 
-    map.events.add('click', (e) => {
-        const coords = e.get('coords');
+    document.addEventListener('click', (e) => {
+        if (e.target.classList.contains("balloon__form-button")) {
+            e.preventDefault();
 
-        reviewsCollections.add(new ymaps.Placemark(coords, {
+            const formNode = e.target.parentNode;
+            const inputNodes = formNode.querySelectorAll('.input__info');
+
+            for (const inputNode of inputNodes) {
+                if (!inputNode.value) {
+                    inputNode.classList.add('empty__field');
+                    return;
+                } else {
+                    inputNode.classList.remove('empty__field');
+                }
+            }
+
+            let geoObjectStorage = JSON.parse(storage.geoObjects || '{}')
+
+            if (geoObjectStorage[geoObjects[geoObjects.length - 1].geometry._coordinates]) {
+                geoObjectStorage[geoObjects[geoObjects.length - 1].geometry._coordinates].push({
+                    name: inputNodes[0].value,
+                    place: inputNodes[1].value,
+                    review: inputNodes[2].value
+                })
+            } else {
+                geoObjectStorage[geoObjects[geoObjects.length - 1].geometry._coordinates] = [{
+                    name: inputNodes[0].value,
+                    place: inputNodes[1].value,
+                    review: inputNodes[2].value
+                }]
+            }
+
+            storage.geoObjects = JSON.stringify(geoObjectStorage);
+            console.log(storage.geoObjects);
+
+            for (const inputNode of inputNodes) {
+                inputNode.value = '';
+            }
+        }
+    })
+
+        map.events.add('click', (e) => {
+            const coords = e.get('coords');
+
+            geoObjects.push(new ymaps.Placemark(coords, {
                 balloonContent: [
                     '<div class="balloon">',
                     '<ul class="balloon__reviews-list">',
@@ -60,10 +137,12 @@ function init() {
                     '</form>',
                     '</div>'
                 ].join(''),
-            }, {balloonMinHeight: 450}))
-    });
+            }, {balloonMinHeight: 450}));
 
-    map.geoObjects.add(reviewsCollections);
-}
+            clusterer.add(geoObjects);
+        });
 
-ymaps.ready(init);
+        map.geoObjects.add(clusterer);
+    }
+
+    ymaps.ready(init);
