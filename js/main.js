7b576da260d3a3d70e9586b91df837559ca3b6a8
Vycheslav Smirnov <smirn.006vychislavoo@gmail.com>
2023-03-10 18:21:31 +0300

    The main functionality has been completed.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const storage = localStorage;
+const myPlacemarks = {};
+let activeCoords;
 
 function init() {
     const map = new ymaps.Map('map', {
@@ -6,10 +8,6 @@ function init() {
             zoom: 12,
             controls: ['zoomControl'],
             behaviors: ['scrollZoom']
-        }),
-        objectManager = new ymaps.ObjectManager({
-            clusterize: true,
-            clusterDisableClickZoom: true
         }),
         clusterer = new ymaps.Clusterer({
             groupByCoordinates: true,
@@ -20,7 +18,16 @@ function init() {
         }),
         geoObjects = [];
 
+    let placemarkId = 0;
+
     const storageGeoObjects = JSON.parse(storage.geoObjects || '{}');
+
+    clusterer.events.add('click', (e) => {
+        const target = e.get('target');
+
+        activeCoords = target.geometry._coordinates.join(',');
+        myPlacemarks[activeCoords].cluster = target;
+    })
 
     for (const coords in storageGeoObjects) {
         let reviewItems = '';
@@ -36,7 +43,8 @@ function init() {
             countPlacemarks++;
         }
 
-        geoObjects.push(new ymaps.Placemark(coords.split(','), {
+        const newPlacemark = new ymaps.Placemark(coords.split(','), {
+            id: ++placemarkId,
             balloonContent: [
                 '<div class="balloon">',
                 '<ul class="balloon__reviews-list">',
@@ -51,7 +59,24 @@ function init() {
                 '</form>',
                 '</div>'
             ].join(''),
-        }, {balloonMinHeight: 450}));
+        }, {balloonMinHeight: 450})
+        // console.log(newPlacemark);
+        // console.log(newPlacemark.properties.get('id'));
+        // console.log(map);
+        const placemarkCoords = newPlacemark.geometry._coordinates.join(',');
+        myPlacemarks[placemarkCoords] = {
+            placemark: newPlacemark,
+            balloonContent: reviewItems,
+            cluster: null
+        }
+
+        newPlacemark.events.add('click', (e) => {
+            const target = e.get('target');
+
+            activeCoords = target.geometry._coordinates.join(',');
+        })
+        geoObjects.push(newPlacemark);
+
         for (let i = 0; i < countPlacemarks - 1; i++) {
             geoObjects.push(new ymaps.Placemark(coords.split(',')));
         }
@@ -90,19 +115,17 @@ function init() {
             let geoObjectStorage = JSON.parse(storage.geoObjects || '{}')
 
             const name = inputNodes[0].value;
-            const place = inputNodes[0].value;
-            const review = inputNodes[0].value;
+            const place = inputNodes[1].value;
+            const review = inputNodes[2].value
 
-            const coords = geoObjects[geoObjects.length - 1].geometry._coordinates
-
-            if (geoObjectStorage[coords]) {
-                geoObjectStorage[coords].push({
+            if (geoObjectStorage[activeCoords]) {
+                geoObjectStorage[activeCoords].push({
                     name: name,
                     place: place,
                     review: review
                 })
             } else {
-                geoObjectStorage[coords] = [{
+                geoObjectStorage[activeCoords] = [{
                     name: name,
                     place: place,
                     review: review
@@ -111,7 +134,7 @@ function init() {
 
             storage.geoObjects = JSON.stringify(geoObjectStorage);
 
-            addReview(name, place, review, coords);
+            addReview(name, place, review, activeCoords);
 
 
             for (const inputNode of inputNodes) {
@@ -139,26 +162,47 @@ function init() {
             ].join(''),
         }, {balloonMinHeight: 450}));
 
+        geoObjects[geoObjects.length - 1].events.add('click', (e) => {
+            let placemarkId = e.get('objectId');
+            console.log(placemarkId);
+        })
+
         clusterer.add(geoObjects);
     });
 
+    function addReview(name, place, review) {
+        myPlacemarks[activeCoords].placemark.properties.set('balloonContent', [
+            '<div class="balloon">',
+            '<ul class="balloon__reviews-list">',
+            myPlacemarks[activeCoords].balloonContent,
+            '<li>',
+            `<span class="item__name">${name}</span>`,
+            `<span class="item__information">${place}</span>`,
+            `<p class="item__review">${review}</p>`,
+            '</li>',
+            '</ul>',
+            '<h3 class="balloon__title">Отзыв:</h3>',
+            '<form class="balloon__form" action="#">',
+            '<input class="input__info" type="text" placeholder="Укажите ваше имя">',
+            '<input class="input__info" type="text" placeholder="Укажите место">',
+            '<textarea  class="input__info input__review" name="review" cols="30" rows="10" placeholder="Оставить отзыв"></textarea>',
+            '<button class="balloon__form-button" type="submit">Добавить</button>',
+            '</form>',
+            '</div>'
+        ].join(''))
+
+        geoObjects.push(new ymaps.Placemark(activeCoords.split(',')));
+        clusterer.add(geoObjects);
+
+        const objectState = clusterer.getObjectState(myPlacemarks[activeCoords].placemark);
+        if (objectState.isClustered) {
+            clusterer.balloon.open(objectState.cluster);
+        } else {
+            myPlacemarks[activeCoords].placemark.balloon.open();
+        }
+    }
+
     map.geoObjects.add(clusterer);
-}
-
-function addReview(name, place, review, coords) {
-    const reviewsListNode = document.querySelector('.balloon__reviews-list');
-
-    const newLiNode = document.createElement('li');
-    newLiNode.classList.add("balloon__reviews-item");
-    newLiNode.innerHTML = [
-        `<span class="item__name">${name}</span>`,
-        `<span class="item__information">${place}</span>`,
-        `<p class="item__review">${review}</p>`].join('')
-
-    reviewsListNode.appendChild(newLiNode);
-
-    geoObjects.push(new ymaps.Placemark(coords));
-    clusterer.add(geoObjects)
 }
 
 ymaps.ready(init);
