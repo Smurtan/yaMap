4a3e5c20ca43f117ec038359671b9612c840c10b
Vycheslav Smirnov <smirn.006vychislavoo@gmail.com>
2023-03-16 18:52:24 +0300

    Added plugins to the project configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
 let path = require('path');
 let rules = require('./webpack.config.rules');
+let HtmlPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         index: './src/index.js'
-    }
+    },
+    output: {
+        filename: "[name].[hash].js",
+        path: path.resolve('dist')
+    },
+    devServer: {
+        index: "index.html",
+        overlay: true
+    },
+    devtool: 'source-map',
+    module: {
+        rules
+    },
+    optimization: {
+
+    },
+    plugins: [
+        new HtmlPlugin({
+            title: "Review Map",
+            template: "index.hbs"
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    mode: process.env.NODE_ENV === "production" ? "production" : "development"
 }
--- webpack.config.rules.js
+++ webpack.config.rules.js
@@ -1,5 +1,25 @@
 module.exports = function () {
     return [
-
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: "babel-loader"
+        },
+        {
+            text: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.hbs/,
+            use: "handlebars-loader"
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg|)$/i,
+            use: 'file-loader?name=images/[hash].[ext]'
+        },
+        {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            use: 'file-loader?name=fonts/[hash].[ext]'
+        }
     ]
 }
